cfe124412c6033ee3606435a4dcde2ddc252ee1d
Johnathan Leppert <johnathan@inspectobot.com>
2022-01-14 07:47:08 +0000

    working FRC controller

--- mecanum_drive_controller.cpp
+++ mecanum_drive_controller.cpp
@@ -25,10 +25,13 @@
 #include <frc/kinematics/MecanumDriveKinematics.h>
 #include <frc/kinematics/MecanumDriveWheelSpeeds.h>
 
+#include <frc/trajectory/Trajectory.h>
 #include <frc/trajectory/TrajectoryGenerator.h>
 #include <frc/trajectory/TrapezoidProfile.h>
 
 #include <frc/geometry/Translation2d.h>
+#include <frc/geometry/Pose2d.h>
+#include "frc/geometry/Rotation2d.h"
 
 #include <units/acceleration.h>
 #include <units/angle.h>
@@ -41,7 +44,11 @@
 
 #include <wpi/numbers>
 
+#include <math.h>
+
 #include <chrono>
+
+#include <inttypes.h>
 
 #define FLT float
 
@@ -84,9 +91,9 @@ static constexpr units::meter_t kTolerance{1 / 12.0};
 static constexpr units::radian_t kAngularTolerance{2.0 * wpi::numbers::pi / 180.0};
  
 constexpr auto kTrackWidth =
-    205_mm;  // Distance between centers of right and left wheels on robot
+    200_mm;  // Distance between centers of right and left wheels on robot
 constexpr auto kWheelBase =
-    205_mm;  // Distance between centers of front and back wheels on robot
+    200_mm;  // Distance between centers of front and back wheels on robot
 
 using radians_per_second_squared_t =
     units::compound_unit<units::radians,
@@ -111,50 +118,95 @@ const frc::MecanumDriveKinematics kDriveKinematics{
     frc::Translation2d(-kWheelBase / 2, -kTrackWidth / 2)
 };
 
-
-/*
-function remapWheels(wheels) {
-      return [
-        wheels[2],
-        wheels[3],
-        wheels[1],
-        wheels[0]
-      ];
-    }
-
-    function linearVelocityToRPM(wheels) {
-      return wheels.map(v => {
-        return (v * 60 / (Math.PI * 2));
-      });
-    }
-
-*/
-
-
 frc::MecanumDriveKinematics m_kinematics{ kDriveKinematics };
+
+struct wheelVelocityMessage {
+  uint32_t timestamp; 
+  int16_t velocity[4];  
+
+  MSGPACK_DEFINE_MAP(timestamp, velocity)
+};
+
+# define TAU M_PI * 2
+
+int16_t velocityToRPM(units::meters_per_second_t speed) {
+  //return int16_t ((speed * 60) / TAU); 
+
+  return int16_t ((60 * speed) / ((45 * 0.001 * M_PI) * 2));
+}
+
+const string WHEEL_VELOCITY_COMMAND_KEY = "rover_wheel_velocity_command";
+
+
+Redis* redis;
+
+void stopWheels() {
+  wheelVelocityMessage message { 0, { 0, 0, 0, 0 } };
+
+  std::stringstream packed;
+  msgpack::pack(packed, message);
+  
+  packed.seekg(0);
+
+  redis->set(WHEEL_VELOCITY_COMMAND_KEY, packed.str()); 
+}
+
+
+frc::Pose2d getCurrentPose() {
+  auto pose = redis->get(POSE_DATA_KEY);
+    
+  string s = *pose;
+  msgpack::object_handle oh = msgpack::unpack(s.data(), s.size());
+
+  msgpack::object deserialized = oh.get();
+
+  PoseMessage poseMessage { 0, { 0, 0, 0 }, { 0, 0, 0, 0 } };
+  deserialized.convert(poseMessage);
+
+  printf("xy: %f %f \n", poseMessage.pos[0] * -1, poseMessage.pos[1] * -1);
+
+  Eigen::Quaternionf orientation = Eigen::Quaternionf(poseMessage.rot[0], poseMessage.rot[1], poseMessage.rot[2], poseMessage.rot[3]);
+  auto euler = orientation.toRotationMatrix().eulerAngles(0, 1, 2);
+  std::cout << "Euler from quaternion in roll, pitch, yaw"<< std::endl << euler << std::endl;
+
+  frc::Rotation2d heading = frc::Rotation2d(units::radian_t (orientation.toRotationMatrix().eulerAngles(0, 1, 2)[2]));
+
+  //std::cout << "pose2d rotation" << std::endl << heading.Radians().value() << std::endl;
+
+  //frc::Rotation2d heading2 = frc::Rotation2d{0_deg};
+
+  frc::Pose2d robotPose{units::meter_t(poseMessage.pos[0] * -1), units::meter_t(poseMessage.pos[1] * -1), heading};
+
+  return robotPose;
+}
 
 int main(int argc, char **argv) {
 	signal(SIGABRT, intHandler);
 	signal(SIGTERM, intHandler);
 	signal(SIGINT, intHandler);
 
-  auto redis = Redis("tcp://127.0.0.1:6379");
+  setbuf(stdout, NULL);
+
+  redis = new Redis("tcp://127.0.0.1:6379");
 
 
   frc::HolonomicDriveController controller{
-      frc2::PIDController{1.0, 0.0, 0.0}, frc2::PIDController{1.0, 0.0, 0.0},
+      frc2::PIDController{1.5, 0.0, 0.0}, 
+
+      frc2::PIDController{1.0, 0.0, 0.0},
+      
       frc::ProfiledPIDController<units::radian>{
-          1.0, 0.0, 0.0,
+          0.1, 0.0, 0.0,
           frc::TrapezoidProfile<units::radian>::Constraints{ kMaxAngularSpeed, kMaxAngularAcceleration } }};
 
-  frc::Pose2d robotPose{0_m, 0_m, frc::Rotation2d{0_deg}};
+  frc::Pose2d robotPose = getCurrentPose();
 
 
-  auto waypoints = std::vector{frc::Pose2d{0.5_m, 0_m, 0_rad},
-                               frc::Pose2d{0_m, 0_m, 0_rad}};
+  auto waypoints = std::vector{robotPose,
+                               frc::Pose2d{0.5_m, 0.0_m, robotPose.Rotation() }};
 
   auto trajectory = frc::TrajectoryGenerator::GenerateTrajectory(
-      waypoints, {0.3_mps, 0.2_mps_sq});
+      waypoints, {0.4_mps, 0.4_mps_sq});
 
   constexpr auto kDt = 0.02_s;
 
@@ -167,6 +219,10 @@ int main(int argc, char **argv) {
   int64_t lastTime = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
   int64_t startTime = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
 
+  printf("total time: %f \n", totalTime);
+
+  frc::Pose2d startingRobotPose = getCurrentPose();
+
   while(keepRunning) {
     timer.waitForNextLoop();
     
@@ -177,17 +233,55 @@ int main(int argc, char **argv) {
 
     int64_t elapsedTime = currentTime - startTime;
 
-    if(totalTime > (units::second_t (elapsedTime * 1000000))) {
-      auto state = trajectory.Sample(units::second_t (dt * 1000000));
+    frc::Pose2d robotPose = getCurrentPose();
+    
+    printf("elapsed time: %ld \n", elapsedTime);
+    //if(totalTime > (units::second_t (elapsedTime / 1000000))) {
+      frc::Trajectory::State state = trajectory.Sample(units::second_t (dt * 1000000));
+     
 
-      auto targetChassisSpeeds = controller.Calculate(robotPose, state, 0_rad);
+      frc::ChassisSpeeds targetChassisSpeeds = controller.Calculate(robotPose, state, startingRobotPose.Rotation());
       
-      auto targetWheelSpeeds = m_kinematics.ToWheelSpeeds(targetChassisSpeeds);
+      frc::MecanumDriveWheelSpeeds targetWheelSpeeds = m_kinematics.ToWheelSpeeds(targetChassisSpeeds);
+      
+      wheelVelocityMessage message = { 
+        uint32_t(elapsedTime), 
+        
+        {
+          velocityToRPM(targetWheelSpeeds.frontRight),
+          velocityToRPM(targetWheelSpeeds.frontLeft),
+          velocityToRPM(targetWheelSpeeds.rearLeft),
+          velocityToRPM(targetWheelSpeeds.rearRight)
+        }
+      };
 
-    }
+      printf("%f %f %f %f", (double)targetWheelSpeeds.frontRight, (double)targetWheelSpeeds.frontLeft, (double)targetWheelSpeeds.rearLeft, (double)targetWheelSpeeds.rearRight);
 
+      std::stringstream packed;
+      msgpack::pack(packed, message);
+  
+      packed.seekg(0);
+
+      //redis->set(WHEEL_VELOCITY_COMMAND_KEY, packed.str()); 
+
+        packed.seekg(0);
+
+        std::string str(packed.str());
+
+        msgpack::object_handle oh =
+        msgpack::unpack(str.data(), str.size());
+
+        msgpack::object deserialized = oh.get();
+        std::cout << deserialized << std::endl;
+
+    //} else {
+    //  stopWheels();
+   // }
 
   }
+
+
+  stopWheels();
 
   double end_time = timer.elapsedTime();
 
